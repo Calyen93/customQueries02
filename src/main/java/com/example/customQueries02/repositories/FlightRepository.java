98ca371d88cd94127e87fcf76cb95aed1ec1f963
Calyen93 <genco.claudio3@gmail.com>
2024-06-17 15:18:26 +0200

    Solution

--- src/main/java/com/example/customQueries02/repositories/FlightRepository.java
+++ src/main/java/com/example/customQueries02/repositories/FlightRepository.java
@@ -2,10 +2,12 @@ package com.example.customQueries02.repositories;
 
 import com.example.customQueries02.models.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
     List<Flight> findAllByStatus(String status);
+    @Query("SELECT f FROM Flight f WHERE f.status = :status1 OR f.status = :status2")
     List<Flight> findByStatusIn(List<String> statuses);
 }
